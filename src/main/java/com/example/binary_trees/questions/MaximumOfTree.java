8eac5f5dcfe53496f75415c9bb48751e3cf1cd4c
darshank-70 <darshankdarsh01@gmail.com>
2026-05-31 18:39:37 +0530

    create more questions

--- src/main/java/com/example/binary_trees/questions/MaximumOfTree.java
+++ src/main/java/com/example/binary_trees/questions/MaximumOfTree.java
@@ -5,7 +5,6 @@ import com.example.binary_trees.Node;
 public class MaximumOfTree {
     public static int max(Node root){
         if(root == null ) return -1;
-
         int leftMax = max(root.left);
         int rightMax = max(root.right);
         return Math.max(leftMax, rightMax);
